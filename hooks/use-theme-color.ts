76f056858f84a2b0fd81cdafacb767d8ed2ed0cf
Asad Ebrahim Khatri <asadkhatri.askh@gmail.com>
2025-10-02 14:50:50 +0500

    improvements

--- hooks/use-theme-color.ts
+++ hooks/use-theme-color.ts
@@ -2,15 +2,7 @@
 import { Colors } from '@/constants/theme';
 
 export function useThemeColor(
-  props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light
 ) {
-  // always use light theme
-  const colorFromProps = props.light;
-
-  if (colorFromProps) {
-    return colorFromProps;
-  } else {
-    return Colors.light[colorName];
-  }
+  return Colors.light[colorName];
 }
